32263cacc43c560ab6d05e770fde6990177ff0a3
minngaeng <minngaeng@gmail.com>
2023-08-26 00:09:22 +0900

    ADD: button onClick_TodoList list

--- wanted_api/src/pages/Todo/Todo.jsx
+++ wanted_api/src/pages/Todo/Todo.jsx
@@ -1,7 +1,82 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const Todo = () => {
+  const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState([]); // 배열 목록
+
+  const onChangeTodoInput = (event) => {
+    setTodoInput(event.currentTarget.value);
+  };
+
+  const addTodo = async () => {
+    try {
+      const response = await axios.post(
+        'https://www.pre-onboarding-selection-task.shop/todos',
+        {
+          todo: todoInput,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(response);
+      setTodos([...todos, response.data]);
+      setTodoInput('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await axios.get(
+          'https://www.pre-onboarding-selection-task.shop/todos',
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+          }
+        );
+        console.log(response);
+        setTodos(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTodos();
+  }, []);
+
   return (
     <div>
-      <div>1</div>
+      <input
+        value={todoInput}
+        onChange={onChangeTodoInput}
+        type="text"
+        placeholder="TODO 내용 입력"
+      />
+      <button onClick={addTodo}>추가</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.todo}</li>
+        ))}
+        <li>
+          <label>
+            <input type="checkbox" />
+            <span>TODO 1</span>
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            <span>TODO 2</span>
+          </label>
+        </li>
+      </ul>
     </div>
   );
 };
